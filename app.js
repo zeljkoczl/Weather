1f1d883088de7f4219006fcbcf8fa0a0533b69cd
zeljkoczl <zeljkoczl@gmail.com>
2018-09-02 18:24:39 +0200

    Weather

---
+++ app.js
@@ -0,0 +1,31 @@
+function weather() {
+  var location = document.getElementById("location");
+  var apiKey = "e265f84c62beba82319772443ef7df14";
+  var url = "https://api.forecast.io/forecast/";
+
+  navigator.geolocation.getCurrentPosition(success, error);
+
+  function success(position) {
+    latitude = position.coords.latitude;
+    longitude = position.coords.longitude;
+
+    location.innerHTML =
+      "Latitude is " + latitude + "° Longitude is " + longitude + "°";
+
+    $.getJSON(
+      url + apiKey + "/" + latitude + "," + longitude + "?callback=?",
+      function(data) {
+        $("#temp").html(data.currently.temperature + "° F");
+        $("#minutely").html(data.minutely.summary);
+      }
+    );
+  }
+
+  function error() {
+    location.innerHTML = "Unable to retrieve your location";
+  }
+
+  location.innerHTML = "Locating...";
+}
+
+weather();
